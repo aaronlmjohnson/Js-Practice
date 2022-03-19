db9cc346a9397cfea5899f35e363fc4ef5d50fc1
Aaron <ajonaaronajon@yahoo.com>
2022-03-19 14:06:10 -0500

    reverseString passes test

--- src/jestTest.js
+++ src/jestTest.js
@@ -2,9 +2,8 @@ export const sentenceCase = (str)=>{
     return str.split('').map((char, i)=> i == 0 ? char.toUpperCase() : char).join('');
 };
 
-
-export const reverseString = ()=>{
-
+export const reverseString = (str)=>{ 
+   return str.split('').reduce((total, char, i)=>total + str[-i + (str.length-1)], '');
 }
 
 export const Calculator = (()=>{
